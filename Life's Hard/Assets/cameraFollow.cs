c3e41304a28725f92b986635938b4d438036d59f
Luke <lukeclogan@gmail.com>
2019-09-14 16:12:37 -0400

    Basic character controller
    implimented character controller
    implimented camera following player

---
+++ Life's Hard/Assets/cameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(PlayerTransform.position, newPos, SmoothFactor);
+        
+    }
+}
